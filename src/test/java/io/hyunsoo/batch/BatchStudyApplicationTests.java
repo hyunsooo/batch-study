5ba48a22be73d8816d234abc0f71125af82ba171
한현수 <dreamh1123@naver.com>
2023-11-25 18:36:03 +0900

    first commit

---
+++ src/test/java/io/hyunsoo/batch/BatchStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hyunsoo.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
